d5de6251d69b26aff1602ac3e348b02346192a93
natedaag <natedaage@yahoo.com>
2018-04-16 15:26:33 -0600

    didn't get pushed earlier

--- src/com/udemy/hospital/donebetter/HospitalManagement1.java
+++ src/com/udemy/hospital/donebetter/HospitalManagement1.java
@@ -1,6 +1,6 @@
 package com.udemy.hospital.donebetter;
 
-public class HospitalManagement1 {
+public class HospitalManagement1 { // all employees can now do their own duties - noone knows or cares.
 
 	public void callUpon(Employee1 emp) {
 		emp.performDuties();
